bdb0386a234a5cf249e457df9b24000e3dd32cdd
sekyeong <sekoonglee@gmail.com>
2025-04-29 20:52:42 +0900

    feat: 카테고리별 가방 평균 사이즈 데이터 추가

---
+++ sokuri/utils/bag_average_size.py
@@ -0,0 +1,10 @@
+CATEGORY_AVERAGE_SIZES = {
+    "백팩": {"width_cm": 30.0, "height_cm": 45.0, "depth_cm": 15.0},
+    "토트백": {"width_cm": 19.0, "height_cm": 13.5, "depth_cm": 13.0},
+    "크로스백": {"width_cm": 25.0, "height_cm": 20.0, "depth_cm": 8.0},
+    "쇼퍼백": {"width_cm": 40.0, "height_cm": 35.0, "depth_cm": 15.0},
+    "클러치백": {"width_cm": 30.0, "height_cm": 20.0, "depth_cm": 3.0},
+    "기타": {"width_cm": 35.0, "height_cm": 28.0, "depth_cm": 12.0},
+    "숄더백": {"width_cm": 35.0, "height_cm": 28.0, "depth_cm": 12.0},
+    "에코백": {"width_cm": 36.0, "height_cm": 36.0, "depth_cm": 10.0},
+}
